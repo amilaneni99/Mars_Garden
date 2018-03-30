235e258c5cc0a6ed22c63c0f75bc5e04e492cf34
amilaneni99 <as.abhi99@gmail.com>
2018-03-30 19:18:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/abhinav/appathon/HomeScreen.java
@@ -0,0 +1,42 @@
+package com.example.abhinav.appathon;
+
+import android.content.Intent;
+import android.media.tv.TvContract;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeScreen extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void navigate(MenuItem item) {
+        Intent nav_intent = new Intent(this,Navigate.class);
+        startActivity(nav_intent);
+    }
+}
